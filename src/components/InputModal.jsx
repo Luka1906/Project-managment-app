daf26fbfef32f72be8c7bd6c3379b913a40d0600
Luka Matovic <lukamatovic1994@gmail.com>
2025-03-14 06:34:04 -0500

    Fixing small bug with dialog prompt

--- src/components/InputModal.jsx
+++ src/components/InputModal.jsx
@@ -15,12 +15,12 @@ export default function InputModal({ ref }) {
     })
     
   return createPortal(
-    <dialog ref={dialog} className="rounded-lg px-6 p-8 shadow-lg flex flex-col gap-4">
-      <h2 className="text-xl font-bold">Invalid Input</h2>
-      <p>Oops...looks like you forgot to enter a value.</p>
-      <p>Please make sure you provide a valid value for every input field</p>
+    <dialog ref={dialog} className="rounded-lg px-6 p-7 shadow-lg ">
+      <h2 className="text-xl font-bold mb-3.5">Invalid Input</h2>
+      <p className="mb-3.5">Oops...looks like you forgot to enter a value.</p>
+      <p className="mb-3.5">Please make sure you provide a valid value for every input field</p>
       <form className="flex justify-end" method="dialog">
-        <button  className="justify-start">Okay</button>
+        <button  className="justify-start bg-stone-400 p-2 px-4 rounded-md">Okay</button>
       </form>
     </dialog>, document.getElementById("modal-root")
   );
